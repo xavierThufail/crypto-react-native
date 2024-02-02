f629f90581211a08c20a40e3d48a6167602d989d
Xavier Thufail <xavier.thufail@traveloka.com>
2024-02-03 02:49:48 +0700

    feat: add new section currency name

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,71 +1,30 @@
 import React from 'react';
-import { StatusBar, SafeAreaView } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { Entypo } from '@expo/vector-icons';
 
 import ChartSection from './chart';
 import OrderSection from './order';
 import { Text, View } from '@/components/Themed';
 import { Link } from 'expo-router';
+import { CurrencyDetail } from '@/hooks';
 
 const Tab = createMaterialTopTabNavigator();
 
-const CurrencyDetail = React.createContext('');
-
 export default function TabLayout() {
+  const { selectedCurrency } = React.useContext(CurrencyDetail);
+
   return (
     <React.Fragment>
-      <View style={{ height: StatusBar.currentHeight }}>
-        <SafeAreaView>
-          <StatusBar translucent />
-        </SafeAreaView>
-      </View>
-      <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
         <Link href={'/search'}>
-          <Text>Bitcoin</Text>
+          <Text>{selectedCurrency?.name}</Text>
         </Link>
+        <Entypo name="chevron-small-down" size={24} color="black" />
       </View>
-      <CurrencyDetail.Provider value={'Bitcoin'}>
-        <Tab.Navigator>
-          <Tab.Screen name="Chart" component={ChartSection} />
-          <Tab.Screen name="ORDER NIH" component={OrderSection} />
-        </Tab.Navigator>
-      </CurrencyDetail.Provider>
+      <Tab.Navigator>
+        <Tab.Screen name="Chart" component={ChartSection} />
+        <Tab.Screen name="Order" component={OrderSection} />
+      </Tab.Navigator>
     </React.Fragment>
-      /* <Tabs
-        screenOptions={{
-          tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-          // Disable the static render of the header on web
-          // to prevent a hydration error in React Navigation v6.
-          headerShown: useClientOnlyValue(false, true),
-        }}>
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: 'Chart NIH',
-            tabBarIcon: undefined,
-            headerRight: () => (
-              <Link href="/modal" asChild>
-                <Pressable>
-                  {({ pressed }) => (
-                    <FontAwesome
-                      name="info-circle"
-                      size={25}
-                      color={Colors[colorScheme ?? 'light'].text}
-                      style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                    />
-                  )}
-                </Pressable>
-              </Link>
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="order"
-          options={{
-            title: 'Order',
-            tabBarIcon: undefined,
-          }}
-        />
-      </Tabs> */
   );
 };
