2fe10721f9416acd8de9ec74a7fc14c1d36a0034
Xavier Thufail <xavier.thufail@traveloka.com>
2024-02-03 02:50:33 +0700

    feat: implement search feature

--- app/search.tsx
+++ app/search.tsx
@@ -1,19 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
+import React from 'react';
+import { StyleSheet, TextInput, ScrollView, TouchableOpacity } from 'react-native';
+import { SvgUri } from 'react-native-svg';
+
 
 import { Text, View } from '@/components/Themed';
+import { CurrencyDetail } from '@/hooks';
+import { Link, router } from 'expo-router';
+import { CurrencyItem } from '@/constants/types';
 
-export default function ModalScreen() {
+const useSearch = () => {
+  const [value, setValue] = React.useState('');
+
+  const { currencies, setSelectedCurrency } = React.useContext(CurrencyDetail);
+
+  const filteredCurrencies = React.useMemo(() => {
+    const valueLowerCase = value.toLowerCase();
+    return currencies.filter((currency) =>
+      currency.name.toLowerCase().includes(valueLowerCase)
+      || currency.symbol.toLowerCase().includes(valueLowerCase));
+  }, [value, currencies]);
+
+  const onCurrencySelect = (currency: CurrencyItem) => () => {
+    setSelectedCurrency(currency);
+    router.replace('/');
+  };
+
+  return {
+    value,
+    setValue,
+    filteredCurrencies,
+    onCurrencySelect
+  };
+};
+
+type HookResult = ReturnType<typeof useSearch>;
+
+const Header = ({ value, setValue }: HookResult) => (
+  <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', gap: 15, marginHorizontal: 15 }}>
+    <View style={{ padding: 5, backgroundColor: '#f4f4f5', borderRadius: 8, flex: 1 }}>
+      <TextInput value={value} onChangeText={setValue} style={{ borderWidth: 0 }} underlineColorAndroid="transparent" />
+    </View>
+    <Link href='/'>
+      <Text style={{ fontWeight: 'bold', color: '#0a68f4' }}>Cancel</Text>
+    </Link>
+  </View>
+);
+
+const CurrencyOption = ({ symbol, name, logo }: CurrencyItem) => (
+  <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingBottom: 15, borderBottomWidth: 1, borderBottomColor: '#f4f4f5', borderStyle: 'solid', paddingHorizontal: 15 }}>
+    <View style={{ flexDirection: 'row', gap: 10, alignItems: 'center' }}>
+      <SvgUri 
+        uri={logo}
+        width={28}
+        height={28}
+        color={'black'}
+      />
+      <Text style={{ fontSize: 16 }}>{name}</Text>
+    </View>
+    <Text style={{ fontSize: 16, color: '#87898c' }}>{symbol}</Text>
+  </View>
+)
+
+const CurrencyList = ({ filteredCurrencies, onCurrencySelect }: HookResult) => (
+  <ScrollView>
+    {filteredCurrencies.map((currency) => (
+      <TouchableOpacity key={currency.symbol} onPress={onCurrencySelect(currency)}>
+        <CurrencyOption {...currency} />
+      </TouchableOpacity>
+    ))}
+  </ScrollView>
+);
+
+export default function SearchScreen() {
+  const hook = useSearch();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Search Page</Text>
-      <Link href={'/'}>
-        <Text>Back</Text>
-      </Link>
-
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+      <Header {...hook} />
+      <CurrencyList {...hook} />
     </View>
   );
 }
@@ -21,8 +85,7 @@ export default function ModalScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    gap: 15
   },
   title: {
     fontSize: 20,
