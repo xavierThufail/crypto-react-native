db0250b1b443a0bce835f8b55bb6b02eace26930
Xavier Thufail <xavier.thufail@traveloka.com>
2024-02-03 19:42:44 +0700

    feat: add common constant

---
+++ constants/common.ts
@@ -0,0 +1,30 @@
+const HEADER = {
+  MODAL: {
+    LABEL: {
+      SELL_PRICE: 'Sell Price',
+      MID_PRICE: 'Mid Price',
+      BUY_PRICE: 'Buy Price',
+    },
+    CONTENT: {
+      TITLE: 'The Price Shown is a Mid Price',
+      DESCRIPTION: 'Mid Price is not buy nor selling price. Final buy and selling price will be calculated when you\'re making transactions.',
+    },
+    BUTTON: {
+      OK: 'Ok, I Understand'
+    }
+  },
+  PRICE: 'Price'
+};
+
+const INTERVAL_CHART = {
+  '24H': '24H',
+  '1W': '1W',
+  '1M': '1M',
+  '1Y': '1Y',
+  'ALL': 'ALL',
+};
+
+export {
+  HEADER,
+  INTERVAL_CHART,
+};
