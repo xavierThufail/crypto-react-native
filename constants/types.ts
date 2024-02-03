7c3a54ffb5b520a9cdf91bcd94030d7106990f85
Xavier Thufail <xavier.thufail@traveloka.com>
2024-02-03 19:46:04 +0700

    feat: update types

--- constants/types.ts
+++ constants/types.ts
@@ -1,32 +1,47 @@
 export type FetchAPIType = <TResponse>(url: string, config?: RequestInit) => Promise<TResponse>;
 
-export type WalletType = {
-  currencyGroup: string;
-  tokenSymbol: string;
-  decimal_point: number;
-  tokenType: string;
-  blockchain: string;
-  explorer: string;
-  listingDate: string;
-  blockchainName: string;
-  logo: string;
-};
-
 export type CurrencyType = {
-  currencyGroup: string;
-  color: string;
-  currencySymbol: string;
-  name: string;
-  logo: string;
-  decimal_point: number;
-  listingDate: string;
-  wallets: WalletType[];
+  id: string;
+  symbol: string;
+  base_currency: string;
+  traded_currency: string;
+  traded_currency_unit: string;
+  description: string;
+  ticker_id: string;
+  volume_precision: number;
+  price_precision: number;
+  price_round: number;
+  pricescale: number;
+  trade_min_base_currency: number;
+  trade_min_traded_currency: number;
+  has_memo: boolean;
+  memo_name: boolean;
+  trade_fee_percent: 0.2;
+  trade_fee_percent_taker: 0.2;
+  trade_fee_percent_maker: 0.1;
+  url_logo: string;
+  url_logo_png: string;
+  is_maintenance: number;
+  is_market_suspended: number;
+  coingecko_id: string;
+  cmc_id: number;
+  name?: string;
 };
 
-export type FetchSupportedCurrenciesResponse = {
-  code: string;
-  message: string;
-  payload: CurrencyType[]
+export type TickerType = {
+  high: string;
+  low: string;
+  vol_btc: string;
+  vol_idr: string;
+  last: string;
+  buy: string;
+  sell: string;
+  server_time: number;
+};
+
+export type FetchSupportedCurrenciesResponse = CurrencyType[];
+export type FetchTickerCurrencyResponse = {
+  ticker: TickerType;
 };
 
 export type CurrencyItem = {
